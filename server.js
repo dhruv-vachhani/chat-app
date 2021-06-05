4afb68fd96e3e1bbeb0d6ba72faf6f80658296c0
dhruv <dhruv.vachhani.dv@gmail.com>
2021-06-05 15:10:01 +0530

    used socket final commit

--- server.js
+++ server.js
@@ -15,3 +15,13 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+const io = require('socket.io')(http)
+
+io.on('connection',(socket) => {
+    console.log(`Connected`);
+
+    socket.on('message' ,(msg) => {
+        socket.broadcast.emit('message',msg)
+    })
+})
